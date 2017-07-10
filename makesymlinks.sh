d933d067337d4927180b70350da2e6a819ee0a61
dropTableUsers42 <rwitaban0001@gmail.com>
2017-07-10 09:12:33 +0530

    First commit of dotfiles

---
+++ makesymlinks.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+###############
+#.make.sh
+# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+###############
+
+###Variables
+
+dir=~/dotfiles
+olddir=~/dotfiles_old
+files  vimrc
+
+################
+
+echo "Creating $olddir for backup of existing dotfiles in ~"
+mkdir -p $olddir
+echo "...done"
+
+echo "Changing to the $dir directory"
+cd $dir
+echo "...done"
+
+for file in $files; do
+	echo "Moving any existing dotfiles from ~ to $olddir"
+	mv ~/.$file ~/dotfiles_old/
+	echo "Creating symlink to $file in home dirctory."
+	ln -s $dir/$file ~/.$file
+done
